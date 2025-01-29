c8b007dd2b3ce837b09ada92420d6f52e5451427
Andrés Ortega <andres03ortega@gmail.com>
2025-01-29 22:46:51 +0100

    segundo apartado web

--- app/page.jsx
+++ app/page.jsx
@@ -11,7 +11,7 @@ function HomePage(){
 
   return(
 
-    <div className="master__body">
+    <div className="">
 
       
       <div className="navbar__master">
@@ -24,6 +24,13 @@ function HomePage(){
           </Link>
         </div>
       </div>
+
+
+
+
+    {/* PRIMERA PARTE */}
+
+      <div className="master__body">
 
 
       <Image className="medworld_full_logo" src={med_logo} alt="Logo de Medworld completo en color azul claro"></Image>
@@ -45,23 +52,64 @@ function HomePage(){
         </div><div className="spacer-s"></div>
 
     
-        <p>Que con tu <span className="destacado-txt-azul">talento</span> y tus años de experiencia sigues luchando por llenar los huecos de tu calendario...</p>
+        <p>Que con tu <span className="azul-claro">talento</span> y tus años de experiencia sigues luchando por llenar los huecos de tu calendario...</p>
 
 
       <div className="linea_vertical"></div>
 
 
       <h2 className="azul-claro"><span className="subrayado">NO</span>, no es justo.</h2>
-      <p>Pero aquí estamos para cambiar esto.</p>
+      <p>Pero aquí estamos para cambiar esto.</p><div className="spacer-m"></div>
+      
 
 
     
 
       <div className="button button_claro">
           <Link href="/contacto">
-            <p>Llámanos</p>
+            <p>Reserva una llamada gratis</p>
           </Link>
       </div> 
+
+      <p className="button_disclaimer_1">Las plazas son limitadas. Sesión valorada en 250 €.</p>
+
+
+
+      </div><div className="spacer-m"></div>
+
+
+
+
+
+    {/* SEGUNDA PARTE */}
+
+    <div className="master__body bg-azul-claro"><div className="spacer-m"></div>
+
+      <h2>El problema que nadie quiere decir en voz alta</h2>
+
+      <div className="linea_vertical_oscuro"></div>
+
+      <p className="azul-oscuro">El marketing tradicional <span className="negrita">te ha vendido humo.</span></p>
+
+      <div className="linea_vertical_oscuro"></div>
+
+      <p className="azul-oscuro">Publicar tres posts al mes en Instagram, pagar anuncios que no atraen pacientes y dejar tu reputación en manos de estrategias genéricas es la receta perfecta para quedarte en el anonimato.</p> <div className="spacer-m"></div>
+
+      <h3 className="azul-oscuro">¿El resultado?</h3> <div className="spacer-xs"></div>
+
+          <div className="box_resultados"><p className="blanco">Más tiempo <span className="negrita">pegado al móvil</span> que al bisturí.</p></div> <div className="spacer-xs"></div>
+
+          <div className="box_resultados"><p className="blanco"><span className="negrita">Más frustración</span> que reconocimiento.</p></div> <div className="spacer-xs"></div>
+
+          <div className="box_resultados"><p className="blanco"><span className="negrita">Menos pacientes</span> y más dudas sobre si este juego vale la pena.</p></div> <div className="spacer-xs"></div>
+
+          <div className="spacer-m"></div>
+
+
+    </div>
+
+
+
 
 
 
